5b9fa5badadf637ac1dd936d433b23e34173eba6
Anvesh H <anveshh@Anveshs-MacBook-Pro.local>
2026-01-08 20:46:41 -0700

    initial

---
+++ src/main/java/com/example/OOPTrainee/OopTraineeApplication.java
@@ -0,0 +1,14 @@
+package com.example.OOPTrainee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopTraineeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopTraineeApplication.class, args);
+		//hello
+	}
+
+}
---
+++ src/test/java/com/example/OOPTrainee/OopTraineeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OOPTrainee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopTraineeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
