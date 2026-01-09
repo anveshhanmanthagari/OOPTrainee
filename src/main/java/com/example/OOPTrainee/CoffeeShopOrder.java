8641380fa77a60c588907fd7816392c5ac90e905
anveshh <ahanmanthagari25@my.trine.edu>
2026-01-08 21:08:24 -0700

    add spotless plugin for code formatting

---
+++ src/main/java/com/example/OOPTrainee/CoffeeShopOrder.java
@@ -0,0 +1,45 @@
+package com.example.OOPTrainee;
+
+import java.util.Scanner;
+
+public class CoffeeShopOrder {
+
+
+    public static void main(String[] args) {
+        //for taking input from user
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of Coffee: ");
+        Integer NumOfCoffeeOrder = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter number of Tea: ");
+        Integer NumOfTeaOrder = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter number of Cake: ");
+        Integer NumOfPastriesOrder = Integer.valueOf(scanner.nextLine());
+        //prices of items
+        Double CoffeePrice = 3.50;
+        Double TeaPrice = 2.75;
+        Double CakePrice = 2.25;
+
+        //calculations with arithmetic operators
+        double subTotal = (NumOfCoffeeOrder * CoffeePrice)
+                + (NumOfTeaOrder * TeaPrice) + (NumOfPastriesOrder * CakePrice);
+        //tax calculation with 7 percentage
+        double tax = subTotal * 0.07;
+        //total calculation
+        double total = subTotal + tax;
+        // printing the receipt with input
+        System.out.println("Coffees: " + NumOfCoffeeOrder);
+        System.out.println("Teas: " + NumOfTeaOrder);
+        System.out.println("Pastries: " + NumOfPastriesOrder);
+        System.out.println("Subtotal: $" + String.format("%.2f", subTotal));
+        System.out.println("Tax: $" + String.format("%.2f", tax));
+        System.out.println("Total: $" + String.format("%.2f", total));
+        //ternary operator to check if total is greater than 15
+        if (total > 15)
+            System.out.println("You qualify for a free drink!");
+        else
+            System.out.println("Add more items to qualify for a free drink!");
+
+
+
+    }
+}
