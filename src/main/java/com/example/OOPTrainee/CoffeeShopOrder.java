b1761be21cb3b24b40c810dc7d999d6ed3bff217
anveshh <ahanmanthagari25@my.trine.edu>
2026-01-10 12:02:51 -0700

    add Javadoc generation task and improve output statements in CoffeeShopOrder

--- src/main/java/com/example/OOPTrainee/CoffeeShopOrder.java
+++ src/main/java/com/example/OOPTrainee/CoffeeShopOrder.java
@@ -2,47 +2,72 @@ package com.example.OOPTrainee;
 
 import java.util.Scanner;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CoffeeShopOrder {
   /*
-     This program calculates the total cost of a coffee shop order including tax.
-     It prompts the user to input the number of coffees, teas, and pastries they want to order.
-     It then calculates the subtotal, tax, and total amount due.
-     If the total amount exceeds $15, it informs the user that they qualify for a free drink.
-
-  */
+   * Coffee Shop Order System
+   * This program calculates the total cost of a coffee shop order including tax.
+   * It prompts the user to enter the number of coffees, teas, and pastries they want to order,
+   * then calculates the subtotal, tax, and total cost.
+   * It also checks if the total exceeds a certain amount to qualify for a free drink.
+   */
 
   public static void main(String[] args) {
-    // for taking input from user
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter number of Coffee: ");
-    Integer NumOfCoffeeOrder = Integer.valueOf(scanner.nextLine());
-    System.out.println("Enter number of Tea: ");
-    Integer NumOfTeaOrder = Integer.valueOf(scanner.nextLine());
-    System.out.println("Enter number of Cake: ");
-    Integer NumOfPastriesOrder = Integer.valueOf(scanner.nextLine());
+
     // prices of items
+    printStatement("Welcome to the Coffee Shop!");
     Double CoffeePrice = 3.50;
     Double TeaPrice = 2.75;
     Double CakePrice = 2.25;
+    printStatement("Item Prices :");
+    printStatement("Coffee: $" + CoffeePrice);
+    printStatement("Tea: $" + TeaPrice);
+    printStatement("Pastries: $" + CakePrice);
+    printStatement("---------------------------");
+    printStatement("Please enter your order details:");
+    // for taking input from user
+    Scanner scanner = new Scanner(System.in);
+    printStatement("Enter number of Coffee: ");
+    Integer NumOfCoffeeOrder = Integer.valueOf(scanner.nextLine());
+    printStatement("Enter number of Tea: ");
+    Integer NumOfTeaOrder = Integer.valueOf(scanner.nextLine());
+    printStatement("Enter number of Pastries: ");
+    Integer NumOfPastriesOrder = Integer.valueOf(scanner.nextLine());
 
     // calculations with arithmetic operators
-    double subTotal =
+    Double subTotal =
         (NumOfCoffeeOrder * CoffeePrice)
             + (NumOfTeaOrder * TeaPrice)
             + (NumOfPastriesOrder * CakePrice);
+
     // tax calculation with 7 percentage
-    double tax = subTotal * 0.07;
+    Double tax = subTotal * 0.07;
+
     // total calculation
-    double total = subTotal + tax;
+    Double total = subTotal + tax;
+
     // printing the receipt with input
-    System.out.println("Coffees: " + NumOfCoffeeOrder);
-    System.out.println("Teas: " + NumOfTeaOrder);
-    System.out.println("Pastries: " + NumOfPastriesOrder);
-    System.out.println("Subtotal: $" + String.format("%.2f", subTotal));
-    System.out.println("Tax: $" + String.format("%.2f", tax));
-    System.out.println("Total: $" + String.format("%.2f", total));
+    printStatement("Coffees: " + NumOfCoffeeOrder);
+    printStatement("Teas: " + NumOfTeaOrder);
+    printStatement("Pastries: " + NumOfPastriesOrder);
+    printStatement("Subtotal: $" + String.format("%.2f", subTotal));
+    printStatement("Tax: $" + String.format("%.2f", tax));
+    printStatement("Total: $" + String.format("%.2f", total));
+
     // ternary operator to check if total is greater than 15
-    if (total > 15) System.out.println("You qualify for a free drink!");
-    else System.out.println("Add more items to qualify for a free drink!");
+    if (total > 15) printStatement("You qualify for a free drink!");
+    else printStatement("Add more items to qualify for a free drink!");
+
+    // closing statements
+    printStatement("Thank you for visiting the Coffee Shop!");
+    printStatement("cashier: Anvesh H \n ID: 1600540");
+    printStatement("---------------------------");
+    scanner.close();
+  }
+
+  public static void printStatement(String printStatement) {
+    System.out.println(printStatement);
   }
 }
